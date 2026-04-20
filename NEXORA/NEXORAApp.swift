3d7ab04ae972ac413e749ac073b575a28fa1a9bf
Brandon Zavala <itzbrandonlee@yahoo.com>
2026-04-20 16:15:07 -0700

    Initial Commit

---
+++ NEXORA/NEXORAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEXORAApp.swift
+//  NEXORA
+//
+//  Created by csuftitan on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct NEXORAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

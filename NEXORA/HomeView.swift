d1c4fb515b70ee207c4208b937d73cd97915e65c
GilbertoB23 <111822725+GilbertoB23@users.noreply.github.com>
2026-04-25 15:48:31 -0700

    Implement search bar in HomeView
    Added a search bar to the HomeView for game searching.

--- NEXORA/HomeView.swift
+++ NEXORA/HomeView.swift
@@ -1,8 +1,41 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text("Home Page")
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [Color("NexoraBlue"), Color("NexoraPurple")],
+                               startPoint: .topLeading,
+                               endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+                
+                VStack {
+                    Text("Welcome to Nexora!")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                        .foregroundColor(.white)
+                    //Search Bar
+                    TextField("Search Games...", text: $searchText)
+                        .padding()
+                        .background(Color(.white))
+                        .opacity(0.6)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                    Spacer()
+                    
+                }
+                //.navigationTitle("Home page")
+            }
+            
+        }
+        
     }
+}
+
+#Preview {
+    HomeView()
 }
 
